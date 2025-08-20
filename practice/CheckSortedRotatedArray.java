935466137dec4493f0c50ea16a62c03c005d59d3
vipinshivhare <vipinshivhare804@gmail.com>
2025-08-20 10:46:22 +0530

    initial

--- practice/CheckSortedRotatedArray.java
+++ practice/CheckSortedRotatedArray.java
@@ -1,10 +1,22 @@
 public class CheckSortedRotatedArray {
+    public static boolean checkRotated(int[] arr){
+        int count = 0;
 
+        for(int i=1;i<arr.length;i++){
+            if(arr[i-1] > arr[i]){
+                count++;
+            }
+        }
+        if(arr[0] < arr[arr.length-1]) count++;
 
+        return count<=1;
+    }
 
 
     public static void main(String[] args) {
-        
+        int[] arr = {3,4,5,1,2};
+        boolean ans = checkRotated(arr);
+        System.out.println(ans);
     }
     
 }
